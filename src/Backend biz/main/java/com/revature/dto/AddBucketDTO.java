2a16fd571dc710b153281d4b1d359602fbede6e7
ColtenMurray <60444923+ColtenMurray@users.noreply.github.com>
2021-09-11 17:21:17 -0500

    Add files via upload

---
+++ src/Backend biz/main/java/com/revature/dto/AddBucketDTO.java
@@ -0,0 +1,37 @@
+package com.revature.dto;
+
+
+
+public class AddBucketDTO {
+	
+	private String name;
+	
+	private String country;
+	
+	private String city;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+}
--- src/Backend biz/main/java/com/revature/dto/AddBucketToUserDTO.java
+++ src/Backend biz/main/java/com/revature/dto/AddBucketToUserDTO.java
@@ -1,5 +1,17 @@
 package com.revature.dto;
 
 public class AddBucketToUserDTO {
+	
+	private int bucketId;
+
+	public int getBucketId() {
+		return bucketId;
+	}
+
+	public void setBucketId(int bucketId) {
+		this.bucketId = bucketId;
+	}
+	
+	
 
 }
