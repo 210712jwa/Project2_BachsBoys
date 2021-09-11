09c9caaa6eec36b14b4889e9d25b47c054f72c5f
ColtenMurray <60444923+ColtenMurray@users.noreply.github.com>
2021-09-11 17:23:04 -0500

    added Bucket User Connection

--- src/Backend biz/main/java/com/revature/model/Bucket.java
+++ src/Backend biz/main/java/com/revature/model/Bucket.java
@@ -24,6 +24,45 @@ public class Bucket {
 	@Column (name = "city")
 	private String city;
 	
+	@ManyToMany(mappedBy = "buckets")
+    private Set<User> users = new HashSet<>();
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	public void addUser(User user) {
+		this.users.add(user);
+	}
+	
 	
 
 }
